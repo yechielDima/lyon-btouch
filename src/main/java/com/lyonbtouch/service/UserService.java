44213d53e8b21e7c38ef96caca72efe41ec4b8e6
יחיאל <yehieldimenshtein@gmail.com>
2026-07-15 01:05:40 +0300

    Remove stray comment; set global RTL direction

--- src/main/java/com/lyonbtouch/service/UserService.java
+++ src/main/java/com/lyonbtouch/service/UserService.java
@@ -103,7 +103,7 @@ public class UserService {
         otpCodeRepository.save(otp);
 
         auditService.log(user.getId(), "OTP_VERIFIED", "Login via OTP for " + phone);
-        user.getQualifications().size(); // Initialize lazy collection
+        user.getQualifications().size();
         return user;
     }
 
